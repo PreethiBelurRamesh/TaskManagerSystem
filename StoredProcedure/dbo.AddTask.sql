fd0e3825a8f1e19951f22b3ef4761410a0e2a037
PreethiBelurRamesh <98536789+PreethiBelurRamesh@users.noreply.github.com>
2022-10-06 12:32:10 +0100

    Adding the stored procedures
    Adding the stored procedures

---
+++ StoredProcedure/dbo.AddTask.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROCEDURE [dbo].[AddTask]
+	@createdDate datetime,
+	@requiredBy datetime,
+	@taskDescription varchar(max),
+	@taskStatus int,
+	@taskType int,
+	@assignedTo int,
+	@comment varchar(max),
+	@dateAdded datetime,
+	@commentType int
+	
+AS
+    DECLARE @taskId int
+    INSERT INTO dbo.Task(CreatedDate,RequiredBy,TaskDescription,TaskStatus,TaskType,AssignedTo)
+	VALUES(@createdDate,@requiredBy,@taskDescription,@taskStatus,@taskType,@assignedTo)
+	SET @taskId = (SELECT max(TaskID) FROM dbo.Task)
+
+	INSERT INTO dbo.Comments(DateAdded,Comment,CommentType,TaskId)
+	Values(@DateAdded,@comment,@commentType,@taskId)
+
---
+++ StoredProcedure/dbo.DeleteById.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[DeleteById]
+	@taskId int 
+AS
+	DELETE FROM dbo.Comments where TaskId = @taskId
+	DELETE FROM dbo.Task where TaskId = @taskId
+
